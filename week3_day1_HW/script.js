aab6b4ba977cb7dd623d96be0afe37e4694905c2
jdbpt <jbrown5.filn@gmail.com>
2023-06-07 09:48:51 -0400

    third commit

--- week3_day1_HW/script.js
+++ week3_day1_HW/script.js
@@ -4,7 +4,52 @@
 
 //1. Define a function expression maxOfThree which takes three numbers as arguments and returns the greatest (Math.max not allowed)
 
+let maxOfThree = function(x, y, z){
+    let max = null;
+    //if x greater than or equal to y and z, return x
+    if(x >= y && x >= z){
+        max = x;
+    }
+    //if y greater than or equal to z and x return y
+    else if(y >= z && y >= x){
+        max = y;
+    }
+    //if z greater than or equal to y and x return z
+    else if(z >= y && z >= x){
+        max = z;
+    } 
+    //else return 0
+    else{
+        max = 0;
+    }
+
+    return max;
+   
+}
+
+console.log(maxOfThree(-1, -2, -5));
+
 //5. Define a function declaration multiplyArray that takes an array of numbers and returns the product of those numbers
+
+//Reference: using Rest parameters: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/rest_parameters
+//https://www.w3docs.com/learn-javascript/rest-parameters-and-spread-syntax.html#spread-syntax
+function multiplyArray(...numbers){
+    //initialize product as 1 to allow for multiplication without alterning value of numbers in array
+    let product = 1;
+
+    for(let i = 0; i < numbers.length; i++){
+        product*=numbers[i];
+    }
+    return product;
+   
+}
+
+//Reference: passing an array using spread syntax: https://www.geeksforgeeks.org/how-to-pass-an-array-as-a-function-parameter-in-javascript/
+console.log(multiplyArray(...[1, 2, 3]));
+
+
+
+
 
 //7. Define a function declaration reverseString that takes a string, reverses it's characters and returns it
 
